722cc762a52087f5def3cee2e76fad34593d04ee
shresth20 <shresthakasera2020@gmail.com>
2024-02-16 20:04:00 +0530

    code added

--- set_9.js
+++ set_9.js
@@ -1,28 +1,29 @@
 // Qs1. Create a new input and button element on the page using JavaScript only. Set text of button to "Click me'
 
 let btn = document.createElement("button");
-let body = document.querySelector("body");
-body.append(btn);
-btn.innerText = "Click me";
-btn.id = "button";
-
-
-// Qs2. Add following attributes to the element :
-
 let inp = document.createElement("input");
-inp.placeholder = "Enter here";
+let body = document.querySelector("body");
+
+body.append(btn);
 body.append(inp);
 
+btn.innerText = "Click me";
+
+// Qs2. Add following attributes to the element :
 // -Change placeholder value of input to "username"
 document.querySelector("input").placeholder = "username";
+// or
+inp = setAttribute("placeholder", "username")
 
 // -Change the id of button to "btn"
 document.getElementById("button").id = "btn";
+// or
+btn = setAttribute("id", "btn")
 
 
 // Qs3. Access the btn using the querySelector and button id. Change the button background color to blue and text color to white.
 
-let chgBtn = document.getElementById("btn");
+let chgBtn = document.querySelector("#btn");
 chgBtn.style.backgroundColor = "blue";
 chgBtn.style.color = "white";
 
@@ -39,6 +40,5 @@ h1.style.color = "purple";
 // Qs5. Create a p tag on the page and set its text to "Apna College Delta Practice", where Delta is bold.
 
 let pg = document.createElement("p");
-pg.innerText = "javascript practicing...";
-pg.style.fontWeight = "bold";
+pg.innerHTML = "<b>javascript</b> practicing...";
 body.append(pg);
