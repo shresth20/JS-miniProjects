9eb1b844356081951ed167a987961277fbafcb8d
shresth20 <shresthakasera2020@gmail.com>
2024-02-26 22:40:48 +0530

    code added

---
+++ mouseOverEffect/app.js
@@ -0,0 +1,27 @@
+console.log("mouseover effects")
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+// ctx.fillStyle = "#FF0000";
+// ctx.fillRect(0, 0, 150, 75);
+
+
+
+
+// ctx.beginPath();
+ctx.arc(95, 50, 40, 0, 2 * Math.PI);
+ctx.stroke();
+
+
+
+
+
+
+
+
+
+
+
+
+
